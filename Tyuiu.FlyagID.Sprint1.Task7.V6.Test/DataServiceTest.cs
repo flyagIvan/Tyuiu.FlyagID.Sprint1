4cb70d6956b570037ba7a4f44e108c0139d61022
Maugli . <flyagid@gmail.com>
2025-11-07 17:09:52 +0500

    Task7

---
+++ Tyuiu.FlyagID.Sprint1.Task7.V6.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.FlyagID.Sprint1.Task7.V6.Lib;
+
+namespace Tyuiu.FlyagID.Sprint1.Task7.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculateExample()
+        {
+            DataService ds = new DataService();
+
+            double x = 2;
+            double y = 1;
+            double expected = Math.Round(Math.Pow(1 + 1 / Math.Pow(2, 2), 2) - 12 * Math.Pow(2, 2) * 1, 3);
+            double result = ds.Calculate(x, y);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.FlyagID.Sprint1.Task7.V6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Tyuiu.FlyagID.Sprint1.Task7.V6.Lib;
+
+namespace Tyuiu.FlyagID.Sprint1.Task7.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Фляг И.Д. | ИСПб-25-1";
+            Console.WriteLine("**");
+            Console.WriteLine("* Спринт #1                                                ");
+            Console.WriteLine(" Тема: Арифметические выражения                           ");
+            Console.WriteLine(" Задание #7                                               ");
+            Console.WriteLine(" Вариант #6                                               ");
+            Console.WriteLine(" Выполнил: Фляг И.Д. | ИСПб-25-1                          ");
+            Console.WriteLine("**");
+            Console.WriteLine(" УСЛОВИЕ:                                                 ");
+            Console.WriteLine(" Вычислить выражение:                                     ");
+            Console.WriteLine(" z = (1 + 1/x^2)^x - 12x^2y                               ");
+            Console.WriteLine(" Ввести значения x и y с клавиатуры.                      ");
+            Console.WriteLine(" Ответ округлить до 3 знаков после запятой.               *");
+            Console.WriteLine("**");
+            Console.Write("Введите значение x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double result = ds.Calculate(x, y);
+
+            Console.WriteLine("**");
+            Console.WriteLine($"Результат: z = {result}");
+            Console.WriteLine("**");
+
+            Console.ReadKey();
+        }
+    }
+}
